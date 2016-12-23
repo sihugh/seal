6871b619ede4c6a64b434f54d844e776f20428f7
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2016-12-23 16:08:50 +0000

    Refactor pull_request_formatter
    Now that we've extracted it we can try and simplify things a bit.

--- lib/pull_request_formatter.rb
+++ lib/pull_request_formatter.rb
@@ -8,24 +8,25 @@ class PullRequestFormatter
 
   def present
     <<-EOF.gsub(/^\s+/, '')
-    #{@index + 1}\) *#{escaped_repo(pull_request)}* | #{escaped_author(pull_request)} | updated #{when_updated(pull_request)}#{thumbs_up(pull_request)}
-    #{labels(pull_request)} <#{escaped_link(pull_request)}|#{escaped_title(pull_request)}> - #{comments(pull_request)}
+    #{@index + 1}\) *#{repo}* | #{author} | updated #{when_updated}#{thumbs_up}
+    #{labels} <#{link}|#{title}> - #{comments}
     EOF
   end
 
 private
 
-  def comments(pr)
-    return "1 comment" if pr["comments_count"] == "1"
-    "#{pr["comments_count"]} comments"
+  def comments_count
+    pull_request["comments_count"].to_i
   end
 
-  def when_updated(pr)
-    days_plural(age_in_days(pr))
+  def comments
+    return "1 comment" if comments_count == 1
+    "#{comments_count} comments"
   end
 
-  def age_in_days(pull_request)
-    (Date.today - pull_request['updated']).to_i
+  def when_updated
+    age_in_days = (Date.today - pull_request['updated']).to_i
+    days_plural(age_in_days)
   end
 
   def days_plural(days)
@@ -39,38 +40,42 @@ private
     end
   end
 
-  def labels(pull_request)
+  def labels
     pull_request['labels']
-      .map { |label| "[#{escaped_label(label)}]" }
+      .map { |label| "[#{format_label(label)}]" }
       .join(' ')
   end
 
-  def thumbs_up(pr)
-    if pr["thumbs_up"].to_i > 0
-      " | #{pr["thumbs_up"].to_i} :+1:"
+  def format_label(label)
+    escape_for_slack(label['name'])
+  end
+
+  def thumbs_up_count
+    pull_request["thumbs_up"].to_i
+  end
+
+  def thumbs_up
+    if thumbs_up_count > 0
+      " | #{thumbs_up_count} :+1:"
     else
       ""
     end
   end
 
-  def escaped_author(pr)
-    escape_for_slack(pr["author"])
+  def author
+    escape_for_slack(pull_request["author"])
   end
 
-  def escaped_label(label)
-    escape_for_slack(label['name'])
+  def link
+    escape_for_slack(pull_request["link"])
   end
 
-  def escaped_link(pr)
-    escape_for_slack(pr["link"])
+  def repo
+    escape_for_slack(pull_request["repo"])
   end
 
-  def escaped_repo(pr)
-    escape_for_slack(pr["repo"])
-  end
-
-  def escaped_title(pr)
-    escape_for_slack(pr["title"])
+  def title
+    escape_for_slack(pull_request["title"])
   end
 
   def escape_for_slack(text)
@@ -78,5 +83,4 @@ private
         .gsub("<", "&lt;")
         .gsub(">", "&gt;")
   end
-
 end
